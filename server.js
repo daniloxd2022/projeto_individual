919bb3001c52c8285b29f7fbd7ad92195c55d133
daniloxd2022 <113191711+daniloxd2022@users.noreply.github.com>
2023-01-10 21:38:38 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
-//server.js
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3200;
 
-const jsonServer = require ('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+server.use(middlewares);
+server.use(router);
 
-server.use(middlewares)
-server.use(router)
-server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+server.listen(port);
